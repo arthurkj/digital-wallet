d6749620d184606c7c667c65e66ee21ae763b431
Arthur Kist Juchem <arthur.juchem@olxbr.com>
2024-07-19 23:39:01 -0300

    Adding more unit tests

---
+++ src/test/java/br/com/akj/digital/wallet/builder/notification/TransactionMessageBuilderTest.java
@@ -0,0 +1,28 @@
+package br.com.akj.digital.wallet.builder.notification;
+
+import br.com.akj.digital.wallet.message.dto.TransactionNotificationMessage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.testcontainers.shaded.org.apache.commons.lang3.RandomUtils.nextLong;
+
+public class TransactionMessageBuilderTest {
+
+    @Test
+    public void build_with_success() {
+        final Long senderId = nextLong();
+        final Long receiverId = nextLong();
+        final BigDecimal amount = BigDecimal.TEN;
+
+        final TransactionNotificationMessage result = TransactionMessageBuilder.build(senderId, receiverId, amount);
+
+        assertNotNull(result);
+        assertEquals(senderId, result.senderId());
+        assertEquals(receiverId, result.receiverId());
+        assertEquals(amount, result.amount());
+    }
+}
